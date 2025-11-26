74a0a750665b210209a567033abc6601a1fa5636
Cesar Calvo Ruiz <23.daw.cesarcalvoruiz@fundacionmontessori.com>
2025-11-26 17:23:51 +0100

    Force HTTPS configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')) {
+        if(\Illuminate\Support\Facades\App::environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
